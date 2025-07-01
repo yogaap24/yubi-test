5600b1f1ccfae25d8f5fa0af4603390c5fba99dd
yogaagungprasetio24 <yogaagungprasetio24@gmail.com>
2025-07-01 12:22:36 +0000

    fix docker

--- yubi-test-backend/docker/entrypoint.sh
+++ yubi-test-backend/docker/entrypoint.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-echo "Checking database connection..."
-while ! mysqladmin ping -h"db" -u"yubi_user" -p"yubi_password" --silent; do
-    echo "Waiting for database connection..."
+echo "Waiting for database connection..."
+while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" --silent; do
     sleep 2
 done
 echo "Database is connected!"
 
-echo "Updating storage permissions..."
 chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
 
-echo "Running database migrations and seeders..."
+echo "Running optimizations and migrations..."
+php artisan optimize:clear
 php artisan migrate --seed --force
 
 echo "Startup script finished. Starting PHP-FPM..."
